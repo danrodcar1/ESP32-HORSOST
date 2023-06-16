c98055e83831469bbad2be27e3f6147bd78be39d
danrodcar1 <74423864+danrodcar1@users.noreply.github.com>
2023-06-16 22:49:24 +0200

    Add files via upload

--- adc_dma_multichan/main/espnow_example_main.c
+++ adc_dma_multichan/main/espnow_example_main.c
@@ -60,37 +60,29 @@ static uint8_t s_example_broadcast_mac[ESP_NOW_ETH_ALEN] = { 0xFF, 0xFF, 0xFF, 0
 
 // VARIABLES
 
-bool waiting;
+bool conv_on;
 
 #define BLINK_GPIO 10
 #define TRIGGER_ADC_PIN 6
 /*---------------------------------------------------------------
         ADC General Macros
 ---------------------------------------------------------------*/
-#define EXAMPLE_READ_LEN   256
-#define EXAMPLE_ADC_CONV_MODE           ADC_CONV_SINGLE_UNIT_1
-
-#if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2
-#define EXAMPLE_ADC_USE_OUTPUT_TYPE1    1
-#define EXAMPLE_ADC_OUTPUT_TYPE         ADC_DIGI_OUTPUT_FORMAT_TYPE1
-#else
-#define EXAMPLE_ADC_OUTPUT_TYPE         ADC_DIGI_OUTPUT_FORMAT_TYPE2
-#endif
-
-static adc_channel_t channel[] = {ADC_CHANNEL_4};
+#define READ_LEN   256
+#define ADC_CONV_MODE           ADC_CONV_SINGLE_UNIT_1
+#define ADC_OUTPUT_TYPE         ADC_DIGI_OUTPUT_FORMAT_TYPE2
+#define FILTER_LEN  15
+#define ADC_ATTEN           ADC_ATTEN_DB_11
 
 static TaskHandle_t s_task_handle;
 //ADC1 Channels
+static adc_channel_t channel[4] = {ADC_CHANNEL_0,ADC_CHANNEL_1,ADC_CHANNEL_2,ADC_CHANNEL_4};
 
-const int ADC1_CHAN[]={ADC_CHANNEL_0,ADC_CHANNEL_1,ADC_CHANNEL_2,ADC_CHANNEL_4};
-int lengthADC1_CHAN= sizeof(ADC1_CHAN) / sizeof(ADC1_CHAN[0]);
-
-#define FILTER_LEN  15
+float EMA_ALPHA = 0.6;
 
 typedef struct{
-	uint32_t channel;
 	uint32_t adc_raw; 					/*4 bytes*/
-	uint32_t voltage;					/*4 bytes*/
+	uint32_t adc_filtered;
+	int voltage;					/*4 bytes*/
 	uint32_t adc_buff[FILTER_LEN];	/*4 bytes*/
 	uint32_t sum;						/*4 bytes*/
 	int AN_i;						/*4 bytes*/
@@ -102,19 +94,7 @@ typedef struct{
 }struct_adclist;
 //static_assert(sizeof(struct_adclist) == 8);
 
-uint32_t AN_Buffer[4][FILTER_LEN] = {0};
-int AN_i = 0;
-int AN_Raw = 0;
-int AN_Filtered = 0;
-
-#define ADC_ATTEN           ADC_ATTEN_DB_11
-
 unsigned long millis = 0;
-
-static int adc_raw[2][10];
-static int voltage[2][10];
-static bool example_adc_calibration_init(adc_unit_t unit, adc_atten_t atten, adc_cali_handle_t *out_handle);
-static void example_adc_calibration_deinit(adc_cali_handle_t handle);
 
 static uint8_t s_led_state = 0;
 
@@ -133,14 +113,14 @@ static void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num, adc
 
     adc_continuous_handle_cfg_t adc_config = {
         .max_store_buf_size = 1024,
-        .conv_frame_size = EXAMPLE_READ_LEN,
+        .conv_frame_size = READ_LEN,
     };
     ESP_ERROR_CHECK(adc_continuous_new_handle(&adc_config, &handle));
 
     adc_continuous_config_t dig_cfg = {
         .sample_freq_hz = 20 * 1000,
-        .conv_mode = EXAMPLE_ADC_CONV_MODE,
-        .format = EXAMPLE_ADC_OUTPUT_TYPE,
+        .conv_mode = ADC_CONV_MODE,
+        .format = ADC_OUTPUT_TYPE,
     };
 
     adc_digi_pattern_config_t adc_pattern[SOC_ADC_PATT_LEN_MAX] = {0};
@@ -148,7 +128,7 @@ static void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num, adc
     for (int i = 0; i < channel_num; i++) {
         uint8_t unit = ADC_UNIT_1;
         uint8_t ch = channel[i] & 0x7;
-        adc_pattern[i].atten = ADC_ATTEN_DB_11;
+        adc_pattern[i].atten = ADC_ATTEN;
         adc_pattern[i].channel = ch;
         adc_pattern[i].unit = unit;
         adc_pattern[i].bit_width = SOC_ADC_DIGI_MAX_BITWIDTH;
@@ -164,30 +144,30 @@ static void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num, adc
 
 static bool check_valid_data(const adc_digi_output_data_t *data)
 {
-#if EXAMPLE_ADC_USE_OUTPUT_TYPE1
-    if (data->type1.channel >= SOC_ADC_CHANNEL_NUM(ADC_UNIT_1)) {
-        return false;
-    }
-#else
-    if (data->type2.channel >= SOC_ADC_CHANNEL_NUM(ADC_UNIT_1)) {
-        return false;
-    }
-#endif
-
-    return true;
+	if (data->type2.channel >= SOC_ADC_CHANNEL_NUM(ADC_UNIT_1)) {
+		return false;
+	}
+	return true;
 }
 
 void blinky(void *pvParameter)
 {
 	gpio_reset_pin(TRIGGER_ADC_PIN);
+	gpio_reset_pin(BLINK_GPIO);
+
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(TRIGGER_ADC_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
     while(1) {
         /* Blink off (output low) */
         gpio_set_level(TRIGGER_ADC_PIN, 0);
-        vTaskDelay(500 / portTICK_PERIOD_MS);
+        gpio_set_level(BLINK_GPIO, 0);
+        conv_on = false;
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
         /* Blink on (output high) */
         gpio_set_level(TRIGGER_ADC_PIN, 1);
+        gpio_set_level(BLINK_GPIO, 1);
+        conv_on = true;
         vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
 }
@@ -307,7 +287,7 @@ static void example_espnow_task(void *pvParameter) {
 			free(recv_cb->data);
 			switch(type & 0b00000011){
 			case NODATA:
-				waiting=false;
+				//waiting=false;
 				ESP_LOGI(TAG,"No hay mensajes MQTT");
 				break;
 			case DATA:
@@ -386,7 +366,7 @@ static esp_err_t example_espnow_init(void) {
 }
 
 
-int readADC_Avg_ORIG(struct_adclist *ADC_Raw, uint32_t chn)
+uint32_t read_adc_avg(struct_adclist *ADC_Raw, int chn)
 {
 	struct_adcread *buf =(struct_adcread *)ADC_Raw->adc_read;
 	buf[chn].sum=0;
@@ -403,126 +383,53 @@ int readADC_Avg_ORIG(struct_adclist *ADC_Raw, uint32_t chn)
 	return (buf[chn].sum/FILTER_LEN);
 }
 
-
-int readADC_Avg(struct_adclist *ADC_Raw, uint32_t chn)
-{
-	struct_adcread *buf =(struct_adcread *)ADC_Raw->adc_read;
-	buf[chn].sum=0;
-
-	buf[chn].adc_buff[buf[chn].AN_i++] = buf[chn].adc_raw;
-	if(buf[chn].AN_i == FILTER_LEN)
+inline void adc_multisampling(struct_adclist *my_reads,adc_digi_output_data_t *p){
+	switch ( p->type2.channel )
 	{
-		buf[chn].AN_i = 0;
-	}
-	for(int i=0; i<FILTER_LEN; i++)
-	{
-		buf[chn].sum += buf[chn].adc_buff[i];
-	}
-	return (buf[chn].sum/FILTER_LEN);
-}
-
-/*
-
-
-static void adc_task(void *pvParameter){
-	struct_adclist *my_reads = (struct_adclist *)pvParameter;
-	while (1) {
-		ESP_LOGI(TAG2,"Turn on sensor supply");
-		for(int i=0;i<my_reads->num;i++){
-			ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC1_CHAN[i], &my_reads->adc_read[i].adc_raw));
-			ESP_LOGI(TAG2, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, ADC1_CHAN[i], my_reads->adc_read[i].adc_raw);
-			if (do_calibration) {
-				ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_handle, readADC_Avg(my_reads,i), &my_reads->adc_read[i].voltage));
-				ESP_LOGI(TAG2, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, ADC1_CHAN[i], my_reads->adc_read[i].voltage);
-			}
-		}
-		vTaskDelay(pdMS_TO_TICKS(1000));
-	}
-	//Tear Down
-	ESP_ERROR_CHECK(adc_oneshot_del_unit(adc1_handle));
-	if (do_calibration) {
-		free(my_reads);
-		example_adc_calibration_deinit(adc1_cali_handle);
+	case 0:
+		my_reads->adc_read[0].adc_raw=p->type2.data;
+		my_reads->adc_read[0].adc_filtered = EMA_ALPHA * my_reads->adc_read[0].adc_raw + (1 - EMA_ALPHA) * my_reads->adc_read[0].adc_filtered;
+		break;
+	case 1:
+		my_reads->adc_read[1].adc_raw=p->type2.data;
+		my_reads->adc_read[1].adc_filtered = EMA_ALPHA * my_reads->adc_read[0].adc_raw + (1 - EMA_ALPHA) * my_reads->adc_read[1].adc_filtered;
+		break;
+	case 2:
+		my_reads->adc_read[2].adc_raw=p->type2.data;
+		my_reads->adc_read[2].adc_filtered = EMA_ALPHA * my_reads->adc_read[0].adc_raw + (1 - EMA_ALPHA) * my_reads->adc_read[2].adc_filtered;
+		break;
+	case 4:
+		my_reads->adc_read[3].adc_raw=p->type2.data;
+		my_reads->adc_read[3].adc_filtered = EMA_ALPHA * my_reads->adc_read[0].adc_raw + (1 - EMA_ALPHA) * my_reads->adc_read[3].adc_filtered;
+		//my_reads->adc_read[3].adc_filtered=read_adc_avg(my_reads,3);
+		break;
 	}
 }
-
-static esp_err_t adc_init(void){
-	//--------Trigger pin for ADC----------//
-	gpio_reset_pin(TRIGGER_ADC_PIN);
-     Set the GPIO as a push/pull output
-    gpio_set_direction(TRIGGER_ADC_PIN, GPIO_MODE_OUTPUT);
-	//-------------ADC1 Init---------------//
-	adc_oneshot_unit_init_cfg_t init_config1 = {
-			.unit_id = ADC_UNIT_1,
-	};
-	ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
-
-	//-------------ADC1 Config---------------//
-	adc_oneshot_chan_cfg_t config = {
-			.bitwidth = ADC_BITWIDTH_DEFAULT,
-			.atten = ADC_ATTEN,
-	};
-	for(int i=0;i<lengthADC1_CHAN;i++){
-		ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC1_CHAN[i], &config));
-	}
-	//-------------ADC1 Calibration Init---------------//
-	do_calibration = example_adc_calibration_init(ADC_UNIT_1, ADC_ATTEN, &adc1_cali_handle);
-	struct_adclist *my_reads = malloc(lengthADC1_CHAN*sizeof(struct_adclist));
-	my_reads->num = lengthADC1_CHAN;
-	my_reads->adc_read = (struct_adcread *)malloc(my_reads->num*sizeof(struct_adcread));
-	for(int i=0;i<my_reads->num;i++){
-		my_reads->adc_read[i].AN_i=0;
-		my_reads->adc_read[i].adc_raw=0;
-		my_reads->adc_read[i].voltage=0;
-		my_reads->adc_read[i].sum=0;
-		for(int j=0;j<FILTER_LEN;j++)my_reads->adc_read[i].adc_buff[j]=0;
-	}
-
-	while (1) {
-		for(int i=0;i<my_reads->num;i++){
-			ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC1_CHAN[i], &my_reads->adc_read[i].adc_raw));
-			ESP_LOGI(TAG2, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, ADC1_CHAN[i], my_reads->adc_read[i].adc_raw);
-			if (do_calibration) {
-				ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_handle, readADC_Avg(my_reads,i), &my_reads->adc_read[i].voltage));
-				ESP_LOGI(TAG2, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, ADC1_CHAN[i], my_reads->adc_read[i].voltage);
-			}
-		}
-		vTaskDelay(pdMS_TO_TICKS(1000));
-	}
-	//Tear Down
-	ESP_ERROR_CHECK(adc_oneshot_del_unit(adc1_handle));
-	if (do_calibration) {
-		free(my_reads);
-		example_adc_calibration_deinit(adc1_cali_handle);
-	}
-
-	//xTaskCreate(&adc_task, "adc_init", 4096, my_reads, 2, &adcTaskHandle);
-	return ESP_OK;
-}
-*/
 
 void init_adc_dma_mode(){
 	esp_err_t ret;
 	uint32_t ret_num = 0;
-	uint8_t result[EXAMPLE_READ_LEN] = {0};
-	memset(result, 0xcc, EXAMPLE_READ_LEN);
+	uint8_t result[READ_LEN] = {0};
+	memset(result, 0xcc, READ_LEN);
 
 
 	s_task_handle = xTaskGetCurrentTaskHandle();
 
 	adc_continuous_handle_t handle = NULL;
 
-	lengthADC1_CHAN = sizeof(channel) / sizeof(adc_channel_t);
+	uint8_t lengthADC1_CHAN = sizeof(channel) / sizeof(adc_channel_t);
 	continuous_adc_init(channel, lengthADC1_CHAN, &handle);
 
 	struct_adclist *my_reads = malloc(lengthADC1_CHAN*sizeof(struct_adclist));
 	my_reads->num = lengthADC1_CHAN;
+	ESP_LOGI(TAG,"ADC Length = %d",lengthADC1_CHAN);
 	my_reads->adc_read = (struct_adcread *)malloc(my_reads->num*sizeof(struct_adcread));
 	for(int i=0;i<my_reads->num;i++){
 		my_reads->adc_read[i].AN_i=0;
 		my_reads->adc_read[i].adc_raw=0;
 		my_reads->adc_read[i].voltage=0;
 		my_reads->adc_read[i].sum=0;
+		my_reads->adc_read[i].adc_filtered=0;
 		for(int j=0;j<FILTER_LEN;j++)my_reads->adc_read[i].adc_buff[j]=0;
 	}
 
@@ -544,16 +451,18 @@ void init_adc_dma_mode(){
 		 */
 		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 
-		while (1) {
-			ret = adc_continuous_read(handle, result, EXAMPLE_READ_LEN, &ret_num, 0);
+		while (conv_on) {
+			ret = adc_continuous_read(handle, result, READ_LEN, &ret_num, 0);
 			if (ret == ESP_OK) {
 				ESP_LOGI("TASK", "ret is %x, ret_num is %"PRIu32, ret, ret_num);
 				for (int i = 0; i < ret_num; i += SOC_ADC_DIGI_RESULT_BYTES) {
 					adc_digi_output_data_t *p = (void*)&result[i];
 					if (check_valid_data(p)) {
-						my_reads->adc_read[p->type2.channel].adc_raw = p->type2.data;
-						ESP_LOGI(TAG, "Unit: %d,_Channel: %d, Struct_value: %x", 1, p->type2.channel, my_reads->adc_read[p->type2.channel].adc_raw);
+						//Multisampling each channel and take average reading
+						adc_multisampling(my_reads,p);
+
 						ESP_LOGI(TAG, "Unit: %d,_Channel: %d, Raw_value: %x", 1, p->type2.channel, p->type2.data);
+						ESP_LOGI(TAG, "Unit: %d,_Channel: %d, Filtered_value: %lu", 1, p->type2.channel, my_reads->adc_read[0].adc_filtered);
 
 					} else {
 						ESP_LOGI(TAG, "Invalid data");
@@ -575,6 +484,7 @@ void init_adc_dma_mode(){
 	ESP_ERROR_CHECK(adc_continuous_stop(handle));
 	ESP_ERROR_CHECK(adc_continuous_deinit(handle));
 }
+
 void app_main(void) {
 	// Initialize NVS
 	esp_err_t ret = nvs_flash_init();
@@ -592,45 +502,4 @@ void app_main(void) {
 
 	//adc_init(NULL);
 	   // xTaskCreate(TaskBlink, "task1", 128, NULL, 1, NULL );
-}
-
-/*---------------------------------------------------------------
-        ADC Calibration
----------------------------------------------------------------*/
-static bool example_adc_calibration_init(adc_unit_t unit, adc_atten_t atten, adc_cali_handle_t *out_handle)
-{
-    adc_cali_handle_t handle = NULL;
-    esp_err_t ret = ESP_FAIL;
-    bool calibrated = false;
-
-    if (!calibrated) {
-        ESP_LOGI(TAG2, "calibration scheme version is %s", "Curve Fitting");
-        adc_cali_curve_fitting_config_t cali_config = {
-            .unit_id = unit,
-            .atten = atten,
-            .bitwidth = ADC_BITWIDTH_DEFAULT,
-        };
-        ret = adc_cali_create_scheme_curve_fitting(&cali_config, &handle);
-        if (ret == ESP_OK) {
-            calibrated = true;
-        }
-    }
-
-    *out_handle = handle;
-    if (ret == ESP_OK) {
-        ESP_LOGI(TAG2, "Calibration Success");
-    } else if (ret == ESP_ERR_NOT_SUPPORTED || !calibrated) {
-        ESP_LOGW(TAG2, "eFuse not burnt, skip software calibration");
-    } else {
-        ESP_LOGE(TAG2, "Invalid arg or no memory");
-    }
-
-    return calibrated;
-}
-
-static void example_adc_calibration_deinit(adc_cali_handle_t handle)
-{
-    ESP_LOGI(TAG2, "deregister %s calibration scheme", "Curve Fitting");
-    ESP_ERROR_CHECK(adc_cali_delete_scheme_curve_fitting(handle));
-
 }
